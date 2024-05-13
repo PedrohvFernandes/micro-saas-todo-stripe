cefe8cff8fb27ba52dd942c93a21d476e7c0f1d6
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-13 17:21:59 -0300

    refactor: delete user-info and refactoring user-dropdown

--- src/app/app/_components/user-dropdown.tsx
+++ src/app/app/_components/user-dropdown.tsx
@@ -1,3 +1,4 @@
+// Como estou usando por exemplo o signOut que é uma função JS então usamos o use client, por estar do lado client
 'use client'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
@@ -17,23 +18,38 @@ import {
   MixerHorizontalIcon,
   RocketIcon,
 } from '@radix-ui/react-icons'
+import { Session } from 'next-auth'
+import { signOut } from 'next-auth/react'
 
-export function UserDropdown() {
+type UserDropdownProps = {
+  user: Session['user']
+}
+
+export function UserDropdown({ user }: UserDropdownProps) {
+  if (!user) return
+  // if (!user.email) return console.error('User email not found')
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
-          variant="ghost"
-          className="relative h-8 flex items-center justify-between w-full space-x-2 !px-0"
+          variant="link"
+          className="relative h-8 flex items-center justify-between w-full space-x-2 !px-0 ring-0 focus-visible:ring-0"
         >
           <Avatar className="size-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarImage src={user.image as string} alt={user.name as string} />
+            {user.email && (
+              <AvatarFallback className="uppercase">
+                {user.email[0]}
+              </AvatarFallback>
+            )}
           </Avatar>
           <div className="flex flex-col flex-1 space-y-1 text-left">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            {user.name && (
+              <p className="text-sm font-medium leading-none">{user.name}</p>
+            )}
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {user.email}
             </p>
           </div>
         </Button>
@@ -41,9 +57,9 @@ export function UserDropdown() {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">{user.name}</p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {user.email}
             </p>
           </div>
         </DropdownMenuLabel>
@@ -59,7 +75,12 @@ export function UserDropdown() {
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        {/* 
+          Se eu usar o signout do next-auth direto aqui vai dar problema, porque isso é uma funcionalidade JS, então eu preciso estar do lado do cliente, por isso colocamos ele dentro de um use client
+
+          Ao clicar em signout ele ja me envia para o auth, como definimos em service>auth
+       */}
+        <DropdownMenuItem onClick={() => signOut()}>
           <ExitIcon className="size-3 mr-3" />
           Log out
         </DropdownMenuItem>
