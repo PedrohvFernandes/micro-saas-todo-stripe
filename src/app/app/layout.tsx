fd68e01ba87517d058372dd24eb45c14a5271cf9
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-13 17:25:39 -0300

    refactor: app page and layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,12 +1,16 @@
 import { PropsWithChildren } from 'react'
 import { MainSidebar } from './_components/main-sidebar'
+import { auth } from '@/services/auth'
 
 // O layout é a onde ditamos como vai ser a casca da nossa pagina, como o header, footer, sidebar, etc.
 // Aqui recebemos o filho do componente, que é o conteudo da pagina. Ou seja, tudo o que esta aqui dentro se propaga para os filhos. Por exemplo alem do header, poderiamos fazer cinco requisições e repassar o conteudo para os filhos sem precisar ficar fazendo requisições em todos os componentes. Ou seja, diminuimos a quantidade de codigo e de requisições entre as paginas
-export default function Layout({ children }: PropsWithChildren) {
+export default async function Layout({ children }: PropsWithChildren) {
+  // Pegamos a sessão do usuário pelo next-auth. Conseguimos pegar o usuário logado e suas informações
+  const session = await auth()
   return (
     <div className="grid grid-cols-[16rem_1fr]">
-      <MainSidebar />
+      {/* Juntando use server com use client(ex: UserInfo) */}
+      <MainSidebar user={session?.user} />
       <main>{children}</main>
     </div>
   )
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,3 @@
-// import { auth } from '@/services/auth'
-// import { UserInfo } from './_components/user-info'
 import {
   DashboardPage,
   DashboardPageHeader,
@@ -8,16 +6,9 @@ import {
 } from '@/components/dashboard/page'
 
 export default async function Page() {
-  // Pegamos a sessão do usuário pelo next-auth. Conseguimos pegar o usuário logado e suas informações
-  // const session = await auth()
-
   // O pre é uma tag do html que serve para mostrar o conteúdo de uma forma mais bonita e organizada em JSON
   // return <pre>{JSON.stringify(session, null, 2)}</pre>
   return (
-    // Juntando use server com use client(ex: UserInfo)
-    // <main className="flex items-center justify-center h-screen">
-    //   <UserInfo user={session?.user} />
-    // </main>
     <DashboardPage>
       <DashboardPageHeader>
         <DashboardPageHeaderTitle>Tarefas</DashboardPageHeaderTitle>
