d076b974e36c032d7aac1c379f55b1046b6bdc16
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-13 16:53:50 -0300

    refactor: refactoring settings page

--- src/app/app/settings/page.tsx
+++ src/app/app/settings/page.tsx
@@ -1,7 +1,19 @@
+import {
+  DashboardPage,
+  DashboardPageHeader,
+  DashboardPageHeaderTitle,
+  DashboardPageMain,
+} from '@/components/dashboard/page'
+
 export default function Page() {
   return (
-    <main className="flex items-center justify-center h-screen">
-      <h1>setting</h1>
-    </main>
+    <DashboardPage>
+      <DashboardPageHeader>
+        <DashboardPageHeaderTitle>Configurações</DashboardPageHeaderTitle>
+      </DashboardPageHeader>
+      <DashboardPageMain>
+        <h1>Configurações</h1>
+      </DashboardPageMain>
+    </DashboardPage>
   )
 }
