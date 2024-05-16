9b38cd6d0581a76bff0a7a90d87e34d509a54bea
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-15 21:58:06 -0300

    refactor: refactoring type todo

--- src/app/app/(main)/types.ts
+++ src/app/app/(main)/types.ts
@@ -1,7 +1,16 @@
-export type Todo = {
-  id: string
-  title: string
-  createdAt: Date
-  updatedAt: Date
-  finishedAt?: Date
-}
+import { ReturnTypeWithoutPromise } from '@/types/return-type-without-promise'
+import { getUserTodos } from './actions'
+
+// Type manual
+// export type Todo = {
+//   id: string
+//   title: string
+//   createdAt: Date
+//   updatedAt: Date
+//   finishedAt?: Date
+// }
+
+// Type de forma automatica, dessa forma eu não preciso me preocupar em manter o tipo Todo atualizado toda vez que mudar no model do prisma
+// Passando o [0] eu pego o tipo do retorno da função getUserTodos, que é um array de Todo, dessa forma eu so retorno o tipo Todo e não o array {}[] e sim {}
+// se não me engano dá pra fazer a mesma coisa usando typescript nativo mesmo com export  type SeuType = Awaited<ReturnType<typeof suaPromisseAqui>>
+export type Todo = ReturnTypeWithoutPromise<typeof getUserTodos>[0]
---
+++ src/types/return-type-without-promise.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// Essa função pega um tipo de função e retorna o tipo de retorno dela sem a Promise, pegando a tipagem do retorno da função
+export type ReturnTypeWithoutPromise<T extends (...args: any) => any> =
+  T extends (...args: any) => Promise<infer U> ? U : never
