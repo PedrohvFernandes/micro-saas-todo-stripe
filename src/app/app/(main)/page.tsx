216d0b4da3384cd7a0fd0bbb9440bc5cb201bf81
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-15 21:16:56 -0300

    refactor: move page app to (main)

--- src/app/app/(main)/page.tsx
+++ src/app/app/(main)/page.tsx
@@ -1,9 +1,14 @@
 import {
   DashboardPage,
   DashboardPageHeader,
+  DashboardPageHeaderNav,
   DashboardPageHeaderTitle,
   DashboardPageMain,
 } from '@/components/dashboard/page'
+import { TodoDataTable } from './_components/todo-data-table'
+import { Button } from '@/components/ui/button'
+import { TodoUpsertSheet } from './_components/todo-upsert-sheet'
+import { PlusIcon } from '@radix-ui/react-icons'
 
 export default async function Page() {
   // O pre é uma tag do html que serve para mostrar o conteúdo de uma forma mais bonita e organizada em JSON
@@ -12,9 +17,17 @@ export default async function Page() {
     <DashboardPage>
       <DashboardPageHeader>
         <DashboardPageHeaderTitle>Tarefas</DashboardPageHeaderTitle>
+        <DashboardPageHeaderNav>
+          <TodoUpsertSheet>
+            <Button variant="outline" size="sm">
+              <PlusIcon className="size-4 mr-3" />
+              Add todo
+            </Button>
+          </TodoUpsertSheet>
+        </DashboardPageHeaderNav>
       </DashboardPageHeader>
       <DashboardPageMain>
-        <h1>Tarefas</h1>
+        <TodoDataTable />
       </DashboardPageMain>
     </DashboardPage>
   )
