3daf5aa6249f967a97d164a6f7ee389ad09745fc
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-17 00:43:44 -0300

    chore: add schema deleteTodoSchema

--- src/app/app/(main)/schema.ts
+++ src/app/app/(main)/schema.ts
@@ -6,8 +6,14 @@ import { z } from 'zod'
 export const upsertTodoSchema = z.object({
   id: z.string().optional(),
   title: z.string().optional(),
-  doneAt: z.date().optional(),
+  doneAt: z.date().optional().nullable(),
 })
 
 // O infer é como se fosse um utilo que conferte o objeto em um tipo
 export type upsertTodoSchema = z.infer<typeof upsertTodoSchema>
+
+export const deleteTodoSchema = z.object({
+  id: z.string(),
+})
+
+export type deleteTodoSchema = z.infer<typeof deleteTodoSchema>
