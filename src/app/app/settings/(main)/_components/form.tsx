dc044c6235443be1e1a2f97ef8447e8b106e9dd2
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-21 17:42:11 -0300

    refactor: component form

--- src/app/app/settings/(main)/_components/form.tsx
+++ src/app/app/settings/(main)/_components/form.tsx
@@ -57,7 +57,7 @@ export function ProfileForm({ defaultValues }: ProfileFormProps) {
   })
   return (
     <Form {...form}>
-      <form onSubmit={onSubmit} className="space-y-8 h-screen">
+      <form onSubmit={onSubmit} className="space-y-8">
         <Card>
           <CardHeader>
             <CardTitle>Name</CardTitle>
