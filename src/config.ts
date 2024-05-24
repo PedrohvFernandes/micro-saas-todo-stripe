780ebfc4692979b1b9ca329be571e47ea3999a79
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2024-05-24 01:28:07 -0300

    chore: add new envs in config

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,17 @@
 export const config = {
   stripe: {
-    publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-    secretKey: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
-    webhookSecret: '',
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    secretKey: process.env.STRIPE_SECRET_KEY,
+    webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
     plans: {
       free: {
-        priceId: process.env.NEXT_PUBLIC_STRIPE_FREE_PLAN_ID,
+        priceId: process.env.STRIPE_FREE_PLAN_ID,
         quota: {
           TASKS: 5,
         },
       },
       pro: {
-        priceId: process.env.NEXT_PUBLIC_STRIPE_PRO_PLAN_ID,
+        priceId: process.env.STRIPE_PRO_PLAN_ID,
         quota: {
           TASKS: 100,
         },
